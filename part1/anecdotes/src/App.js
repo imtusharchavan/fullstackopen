cefc06dee83eac889f4715d33b53222beb0ff4c4
Tushar Chavan <tusharrchavan014@gmail.com>
2023-05-04 00:12:49 +0530

    Ex 1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -13,16 +13,27 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(
+    new Uint8Array(anecdotes.length)
+  );
 
   const random = () => Math.floor(Math.random() * anecdotes.length + 0);
 
   const handleNext = () => setSelected(random);
 
-  console.log(selected)
+  const handleVotes = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+  }
 
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={handleVotes}>
+        vote
+      </button>
       <button onClick={handleNext}>
         next anecdote
       </button>
