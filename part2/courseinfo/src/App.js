514d6a50e19783e25f8fe0ac6bb00bc52fd267e9
Tushar Chavan <tusharrchavan014@gmail.com>
2023-05-07 17:26:11 +0530

    Ex 2.1

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,67 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+
+const Part = ({part}) => {
+  return (
+    <div>
+      <p>{part.name} {part.exercises}</p>
+    </div>
+  )
+}
+
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </div>
+  )
+}
+
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+
+export default App;
